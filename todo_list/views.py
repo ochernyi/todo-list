cfce26b7cc7147cc155d2fd1ffce2e4dcb88d24c
Oleh Chornyi <ochernyi04@gmail.com>
2023-03-18 03:53:15 +0200

    Implement views

--- todo_list/views.py
+++ todo_list/views.py
@@ -1,6 +1,30 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
+
+from todo_list.forms import TaskForm
+from todo_list.models import Task, Tag
 
 
-def index(request):
-    return HttpResponse("HELLO")
+class TaskListView(generic.ListView):
+    model = Task
+    context_object_name = "task_list"
+    paginate_by = 10
+    template_name = "todo_list/index.html"
+
+
+class TaskCreateView(generic.CreateView):
+    model = Task
+    form_class = TaskForm
+    success_url = reverse_lazy("todo_list:todo")
+
+
+class TagListView(generic.ListView):
+    model = Tag
+    context_object_name = "tag_list"
+    paginate_by = 10
+
+
+class TagCreateView(generic.CreateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("todo_list:tag-list")
