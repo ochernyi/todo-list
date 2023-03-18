4be3a8b6da2061dd919febcaded11154f230c167
Oleh Chornyi <ochernyi04@gmail.com>
2023-03-18 03:53:46 +0200

    Create necessary urls

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
-from todo_list.views import index
+from todo_list.views import TaskListView, TagListView, TaskCreateView, TagCreateView
 
 urlpatterns = [
-    path("", index, name="todo")
+    path("", TaskListView.as_view(), name="todo"),
+    path("create/", TaskCreateView.as_view(), name="task_create"),
+    path("tags/", TagListView.as_view(), name="tag-list"),
+    path("tags/create/", TagCreateView.as_view(), name="tag-create"),
 ]
 
 app_name = "todo_list"
